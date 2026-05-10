98e2afb4f5a20634818bbfb3671bcca265b72bbd
maria7987897 <denimd240@mail.ru>
2026-05-10 23:28:15 +0300

    IFileManager.cs

--- Lab10/IFileManager.cs
+++ Lab10/IFileManager.cs
@@ -1,1 +1,29 @@
+namespace Lab10
+{
+    public interface IFileManager
+    {
+        string FolderPath
+        {
+            get; 
+        }
 
+        string FileName
+        {
+            get; 
+        }
+
+        string FileExtension
+        {
+            get; 
+        }
+
+        string FullPath
+        {
+            get; 
+        }
+        
+        void SelectFolder(string path);
+        void ChangeFileName(string name);
+        void ChangeFileFormat(string extension);
+    }
+}
