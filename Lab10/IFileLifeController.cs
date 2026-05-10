2f138d9d516d74d4b47ca0d3e61516ffeacb5a41
maria7987897 <denimd240@mail.ru>
2026-05-10 23:26:50 +0300

    IFileLifeController.cs

--- Lab10/IFileLifeController.cs
+++ Lab10/IFileLifeController.cs
@@ -1,1 +1,10 @@
-
+namespace Lab10
+{
+    public interface IFileLifeController
+    {
+        void CreateFile();
+        void DeleteFile();
+        void EditFile(string content);
+        void ChangeFileExtension(string newExtension);
+    }
+}
