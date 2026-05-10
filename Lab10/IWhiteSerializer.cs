8c83b844d5530f66ec3d95d283ebbb4a9514829e
maria7987897 <denimd240@mail.ru>
2026-05-10 23:29:27 +0300

    IWhiteSerializer.cs

--- Lab10/IWhiteSerializer.cs
+++ Lab10/IWhiteSerializer.cs
@@ -1,1 +1,11 @@
+using Lab9.White;
+using Lab10.White;
 
+namespace Lab10.White
+{
+    public interface IWhiteSerializer
+    {
+        void Serialize(Lab9.White.White obj);
+        Lab9.White.White Deserialize();
+    }
+}
